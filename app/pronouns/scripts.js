09d18c86d75403675e46db04b089f1b1f178591a
Schlafhase <106097366+Schlafhase@users.noreply.github.com>
2024-06-03 23:45:22 +0200

    Add files via upload

---
+++ app/pronouns/scripts.js
@@ -0,0 +1,9 @@
+function updatePronouns() {
+    var e = document.getElementById("pronounSelection");
+    var smallText = document.getElementById("smallText");
+    var pronounIndex = parseInt(e.value);
+    var pronouns = [["They", "Their", "them", "like"], ["She", "Her", "her", "likes"], ["They", "Her", "him", "like"], ["He", "His", "him", "likes"], ["Them", "They", "their"], ["Bom", "Bies", "bie", "likes"], ["<input type=\"text\" size=\"5\"></input>", "<input type=\"text\" size=\"5\"></input>", "<input type=\"text\" size=\"5\"></input>", "likes"]];
+    pronouns[7] = [pronouns[Math.floor(Math.random() * pronouns.length)][0], pronouns[Math.floor(Math.random() * pronouns.length)][1], pronouns[Math.floor(Math.random() * pronouns.length)][2], pronouns[Math.floor(Math.random() * pronouns.length)][3]]
+    
+    smallText.innerHTML = `${pronouns[pronounIndex][0]} ${pronouns[pronounIndex][3]} coding. ${pronouns[pronounIndex][1]} code is very messy sometimes. Ndim is an esoteric programming language created by ${pronouns[pronounIndex][2]}.`;
+}

e23703d224656846ba4482f9d35ec343b4c8f272
Schlafhase <schlafhase190@gmail.com>
2024-06-04 00:16:15 +0200

    update pronoun css

--- app/pronouns/scripts.js
+++ app/pronouns/scripts.js
@@ -5,5 +5,5 @@ function updatePronouns() {
     var pronouns = [["They", "Their", "them", "like"], ["She", "Her", "her", "likes"], ["They", "Her", "him", "like"], ["He", "His", "him", "likes"], ["Them", "They", "their"], ["Bom", "Bies", "bie", "likes"], ["<input type=\"text\" size=\"5\"></input>", "<input type=\"text\" size=\"5\"></input>", "<input type=\"text\" size=\"5\"></input>", "likes"]];
     pronouns[7] = [pronouns[Math.floor(Math.random() * pronouns.length)][0], pronouns[Math.floor(Math.random() * pronouns.length)][1], pronouns[Math.floor(Math.random() * pronouns.length)][2], pronouns[Math.floor(Math.random() * pronouns.length)][3]]
     
-    smallText.innerHTML = `${pronouns[pronounIndex][0]} ${pronouns[pronounIndex][3]} coding. ${pronouns[pronounIndex][1]} code is very messy sometimes. Ndim is an esoteric programming language created by ${pronouns[pronounIndex][2]}.`;
+    smallText.innerHTML = `<span class="pronoun">${pronouns[pronounIndex][0]}</span> ${pronouns[pronounIndex][3]} coding. <span class="pronoun">${pronouns[pronounIndex][1]}</span> code is very messy sometimes. Ndim is an esoteric programming language created by <span class="pronoun">${pronouns[pronounIndex][2]}</span>.`;
 }

781d5613f1dd3c6b86e90b22c9ea1395f1663979
Schlafhase <schlafhase190@gmail.com>
2024-07-17 11:30:17 +0200

    html base tag test 2

---
+++ app/load_correct_base_path.js
@@ -0,0 +1,14 @@
+var base_tag = document.getElementsByTagName("base")[0];
+
+if (locationType() == 0) {
+    base_tag.href = "file:///C:/Projects/websites/schlafhaseDotUk/app/";
+}
+else {
+    base_tag.href = "https://schlafhase.uk";
+}
+
+function locationType(){
+    if( window.location.protocol == 'file:' ){ return 0; }
+    if( !window.location.host.replace( /localhost|127\.0\.0\.1/i, '' ) ){ return 2; }
+    return 1;
+}

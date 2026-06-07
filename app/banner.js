b5bd883efde7c843f9e7e6d3009131c4c9b78c96
Linus <linus.schneeberg@gmail.com>
2026-06-07 13:06:30 +0200

    fix js files and reformat

--- app/banner.js
+++ app/banner.js
@@ -3,33 +3,52 @@ var title = document.getElementById("title");
 banner.width = parseInt(banner.offsetWidth);
 banner.height = parseInt(banner.offsetHeight);
 
-var colors = ["green", "yellow", "magenta", "pink", "red", "orange", "blue", "lightblue", "lime"]
-vr rects = [];
-for (var i = 0; i<50;i++) {
-    rects.push(new Circle((Math.random() * 500) + 50, Math.random()* parseInt(banner.offsetWidth), Math.random() * parseInt(banner.offsetHeight), colors[Math.round(Math.random()*colors.length)], Math.random()/2, Math.random()*2-1, Math.random()*2-1));
+var colors = [
+  "green",
+  "yellow",
+  "magenta",
+  "pink",
+  "red",
+  "orange",
+  "blue",
+  "lightblue",
+  "lime",
+];
+var rects = [];
+for (var i = 0; i < 50; i++) {
+  rects.push(
+    new Circle(
+      Math.random() * 500 + 50,
+      Math.random() * parseInt(banner.offsetWidth),
+      Math.random() * parseInt(banner.offsetHeight),
+      colors[Math.round(Math.random() * colors.length)],
+      Math.random() / 2,
+      Math.random() * 2 - 1,
+      Math.random() * 2 - 1,
+    ),
+  );
 }
 
-
 setInterval(updateCanvas, 30);
-document.addEventListener("scroll", scrollEventHandler)
+document.addEventListener("scroll", scrollEventHandler);
 
 function updateCanvas() {
-    banner.width = parseInt(banner.offsetWidth);
-    banner.height = parseInt(banner.offsetHeight);
-    var ctx = banner.getContext("2d");
-    for (var i = 0; i<rects.length; i++) {
-        var circle = rects[i];
-        circle.applyVelocity();
-        circle.render(banner);
-    }
-    Circle.updateVelMultiplier();
-    title.style.opacity = 1 - (document.documentElement.scrollTop/300);
-    // circle1.applyVelocity();
-    // circle1.render(banner);
-    // circle2.applyVelocity();
-    // circle2.render(banner);
+  banner.width = parseInt(banner.offsetWidth);
+  banner.height = parseInt(banner.offsetHeight);
+  var ctx = banner.getContext("2d");
+  for (var i = 0; i < rects.length; i++) {
+    var circle = rects[i];
+    circle.applyVelocity();
+    circle.render(banner);
+  }
+  Circle.updateVelMultiplier();
+  title.style.opacity = 1 - document.documentElement.scrollTop / 300;
+  // circle1.applyVelocity();
+  // circle1.render(banner);
+  // circle2.applyVelocity();
+  // circle2.render(banner);
 }
 
 function scrollEventHandler() {
-    Circle.setVelMultiplier(10);
+  Circle.setVelMultiplier(10);
 }
--- app/canvas_library/Rectangle.js
+++ app/canvas_library/Rectangle.js
@@ -1,73 +1,73 @@
 class Rectangle {
-    static velMultiplier = 1;
-    constructor(x, y, width, height, color, alpha,  xVel = 0, yVel = 0) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.color = color;
-        this.alpha = alpha;
-        this.angle = 0;
-        this.xVel = xVel;
-        this.yVel = yVel;
-        this.class = Rectangle;
-    }
+  static velMultiplier = 1;
+  constructor(x, y, width, height, color, alpha, xVel = 0, yVel = 0) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.alpha = alpha;
+    this.angle = 0;
+    this.xVel = xVel;
+    this.yVel = yVel;
+    this.class = Rectangle;
+  }
 
-    render(canvas, loop=true, glow=false) {
-        var ctx = canvas.getContext("2d");
-        if (glow) {
-            ctx.shadowBlur = 50;
-            ctx.shadowColor = this.color;
-        }
-        ctx.globalAlpha = this.alpha;
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        if (loop) {
-            if (this.x - this.width - 50 > canvas.width) {
-                this.x = -this.width
-            }
-            if (this.x + this.width + 50 < 0) {
-                this.x = canvas.width + this.width;
-            }
-            if (this.y - 50> canvas.height) {
-                this.y = -this.height - 50;
-            }
-            if (this.y + this.height + 50 < 0) {
-                this.y = canvas.height + 50;
-            }
-        }
+  render(canvas, loop = true, glow = false) {
+    var ctx = canvas.getContext("2d");
+    if (glow) {
+      ctx.shadowBlur = 50;
+      ctx.shadowColor = this.color;
     }
+    ctx.globalAlpha = this.alpha;
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    if (loop) {
+      if (this.x - this.width - 50 > canvas.width) {
+        this.x = -this.width;
+      }
+      if (this.x + this.width + 50 < 0) {
+        this.x = canvas.width + this.width;
+      }
+      if (this.y - 50 > canvas.height) {
+        this.y = -this.height - 50;
+      }
+      if (this.y + this.height + 50 < 0) {
+        this.y = canvas.height + 50;
+      }
+    }
+  }
 
-    setPos (x, y) {
-        this.x = x;
-        this.y = y;
-    }
+  setPos(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    move (x, y) {
-        this.x += x;
-        this.y += y;
-    }
+  move(x, y) {
+    this.x += x;
+    this.y += y;
+  }
 
-    rotate (angle) {
-        this.angle += angle;
-    }
+  rotate(angle) {
+    this.angle += angle;
+  }
 
-    applyVelocity() {
-        this.x += this.xVel * this.class.velMultiplier;
-        this.y += this.yVel * this.class.velMultiplier;
-        this.angle += this.rotVel;
-    }
+  applyVelocity() {
+    this.x += this.xVel * this.class.velMultiplier;
+    this.y += this.yVel * this.class.velMultiplier;
+    this.angle += this.rotVel;
+  }
 
-    static setVelMultiplier(newVelMultiplier) {
-        Rectangle.velMultiplier = newVelMultiplier;
-    }
+  static setVelMultiplier(newVelMultiplier) {
+    Rectangle.velMultiplier = newVelMultiplier;
+  }
 
-    static updateVelMultiplier() {
-        if (Rectangle.velMultiplier > 1) {
-            Rectangle.velMultiplier = Rectangle.velMultiplier * 0.95;
-        }
-        else if (Rectangle.velMultiplier != 1) {
-            Rectangle.velMultiplier = 1;
-        }
+  static updateVelMultiplier() {
+    if (Rectangle.velMultiplier > 1) {
+      Rectangle.velMultiplier = Rectangle.velMultiplier * 0.95;
+    } else if (Rectangle.velMultiplier != 1) {
+      Rectangle.velMultiplier = 1;
     }
+  }
 }
+
